48eaab76759ce3ab9ad23681b883a0c2850d9f01
Christopher Rollins <christopher.rollins@live.com>
2025-04-10 13:55:52 +0200

    feat: remove need for devextreme npm package

--- app/src/javascripts/devextreme.js
+++ app/src/javascripts/devextreme.js
@@ -1,11 +1,3 @@
-import Globalize from 'globalize';
-import 'devextreme/localization/globalize/core';
-import 'devextreme/localization/globalize/number';
-import 'devextreme/localization/globalize/date';
-import 'devextreme/localization/globalize/message';
-import 'devextreme/localization/globalize/currency';
-window.Globalize = Globalize;
-
 import '../../assets/javascripts/data_table';
 import '../../assets/javascripts/data_table_templates';
 import '../../assets/javascripts/master_detail';
